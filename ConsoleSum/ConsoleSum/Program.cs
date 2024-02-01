96c3de993d6920c327542ada6d9433182fdd890a
Tymofiy Regenel <timothy.rehenel@gmail.com>
2024-02-01 18:39:49 +0200

    add gitignore and Console project

---
+++ ConsoleSum/ConsoleSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+var a = ValidateInt("Enter integer А: ", "Incorrect value! Try again");
+var b = ValidateInt("Enter integer B: ", "Incorrect value! Try again");
+
+WriteLine($"\n{a} + {b} = {a + b}");
+WriteLine("\nPress any key to exit.");
+ReadLine();
+return;
+
+int ValidateInt(string message, string errorMessage)
+{
+    int result;
+    bool isOk;
+    do
+    {
+        Write(message);
+        isOk = int.TryParse(ReadLine(), out result);
+        if (!isOk) WriteLine(errorMessage);
+    } while (!isOk);
+
+    return result;
+}
